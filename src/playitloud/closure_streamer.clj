890108618a99e9b19a1717c8b69f9019190f3c04
Holger Schauer <holger.schauer@gmx.de>
2016-02-12 18:13:03 +0100

    Add closure example for stateful functions.

---
+++ src/playitloud/closure_streamer.clj
@@ -0,0 +1,17 @@
+(ns playitloud.closure-streamer)
+
+(defn- transmit [connection data]
+  (println (str "Transmitting data " data)))
+
+(defn- connect [options source]
+  (println "Connecting to " (:name options))
+  {:options options
+   :status :connected})
+
+(defn blare [connection-config]
+  (let [connection (connect connection-config " closure streamer")]
+    (fn [noise]
+      (let [result (str "Streaming " noise " to device " connection)]  
+        (println result)
+        (transmit connection noise)
+        result))))
